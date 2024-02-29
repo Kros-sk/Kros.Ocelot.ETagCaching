5d80fdeb6f0ab40d90ae7243c262c51ceba36978
Milan Martiniak <mino.martiniak@gmail.com>
2024-02-29 10:23:14 +0100

    Composite policy

--- src/Kros.Ocelot.ETagCaching/Policies/CompositePolicy.cs
+++ src/Kros.Ocelot.ETagCaching/Policies/CompositePolicy.cs
@@ -1,19 +1,30 @@
 ﻿namespace Kros.Ocelot.ETagCaching.Policies;
 
-internal sealed class CompositePolicy : IETagCachePolicy
+internal sealed class CompositePolicy(IEnumerable<IETagCachePolicy> eTagCachePolicies) : IETagCachePolicy
 {
-    public ValueTask CacheETagAsync(ETagCacheContext context, CancellationToken cancellationToken)
+    private readonly IEnumerable<IETagCachePolicy> _eTagCachePolicies = eTagCachePolicies;
+
+    public async ValueTask CacheETagAsync(ETagCacheContext context, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        foreach (var policy in _eTagCachePolicies)
+        {
+            await policy.CacheETagAsync(context, cancellationToken);
+        }
     }
 
-    public ValueTask ServeNotModifiedAsync(ETagCacheContext context, CancellationToken cancellationToken)
+    public async ValueTask ServeNotModifiedAsync(ETagCacheContext context, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        foreach (var policy in _eTagCachePolicies)
+        {
+            await policy.ServeNotModifiedAsync(context, cancellationToken);
+        }
     }
 
-    public ValueTask ServeDownstreamResponseAsync(ETagCacheContext context, CancellationToken cancellationToken)
+    public async ValueTask ServeDownstreamResponseAsync(ETagCacheContext context, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        foreach (var policy in _eTagCachePolicies)
+        {
+            await policy.ServeDownstreamResponseAsync(context, cancellationToken);
+        }
     }
 }
